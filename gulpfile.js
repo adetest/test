0bb1706ff12a76ae814046c57c486b47c2eb0559
isLishude <lishude@aliyun.com>
2017-06-02 22:28:01 +0800

    opti

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 const gulp = require('gulp');
-const $ = require('gulp-load-plugins')();
+const gulpLoadPlugins = require('gulp-load-plugins');
 const browserSync = require('browser-sync').create();
-const styles = require('gulp-clean-css');
-const scripts = require('gulp-uglify');
+const sass = require('gulp-ruby-sass');
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
 
 gulp.task('styles', () => {
-    return gulp.src('style/*.css')
-        .pipe(styles())
+    return gulp.src('app/style/*.css')
+        .pipe($.cssnano())
         .pipe(gulp.dest('dist/style'))
         .pipe(browserSync.stream());
 })
 
 gulp.task('scripts', ['styles', 'scripts'], () => {
-   return gulp.src(['js/*.js'])
-        .pipe(scripts())
+    return gulp.src(['app/js/*.js'])
+        .pipe($.uglify())
         .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.stream());
 })
@@ -22,17 +24,28 @@ gulp.task('scripts', ['styles', 'scripts'], () => {
 gulp.task('serve', () => {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "app/"
         }
     });
 
-    gulp.watch('style/*', ['styles']);
+    gulp.watch('app/style/*', ['styles']);
 
-    gulp.watch('js/*', ['scripts']);
+    gulp.watch('app/js/*', ['scripts']);
 
-    gulp.watch("index.html").on('change', browserSync.reload);
+    gulp.watch("app/index.html").on('change', reload({
+        stream: true
+    }));
 })
 
 gulp.task('default', ['serve'], () => {
     console.log('running!')
+})
+
+gulp.task('test', () => {
+    return gulp.src('app/style/index.scss')
+        .pipe($.sourcemaps.init())
+        .pipe($.sass().on('error',sass.logError))
+        .pipe($.cssnano())
+        .pipe($.sourcemaps.write('.'))        
+        .pipe(gulp.dest('dist/style/'))
 })
